0bfb843e75494b7b39b882c511f863f12c7e556c
Levi <levi@teklife.info>
2015-07-12 18:59:33 -0400

    Adding timezone :)

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
+date_default_timezone_set("American/New_York");
+
 define("DS", DIRECTORY_SEPARATOR);
 require_once('..' . DS . 'vendor' . DS . 'autoload.php');
 
