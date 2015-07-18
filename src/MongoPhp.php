b71dd65bce8c4e9a1869cc30fe6f4f45c444601e
Levi <levi@teklife.info>
2015-07-17 20:09:45 -0400

    Added config file, travis will fail, but still working on it

--- src/MongoPhp.php
+++ src/MongoPhp.php
@@ -1,5 +1,5 @@
 <?php namespace teklife;
-
+require_once('..' . DIRECTORY_SEPARATOR . 'config.php');
 
 class MongoPhp
 {
@@ -8,7 +8,15 @@ class MongoPhp
 
     public function __construct()
     {
-        $this->m = new \MongoClient();
+        try {
+            $this->m = new \MongoClient("mongodb://" . MONGO_DB_UN . ":" . MONGO_DB_PW . "@" . MONGO_DB_IP . "/Website");
+        } catch(\Exception $e) {
+            echo "Couldn't connect.";
+            exit;
+        } catch(\MongoConnectionException $e) {
+            echo "Couldn't connect.";
+            exit;
+        }
         $this->db = $this->m->Website;
     }
 
